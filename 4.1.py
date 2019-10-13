8674b537d5d961b362e35c2059eca6e1831b8d85
Aokht17 <Waterdrop99@mail.ru>
2019-10-13 17:59:42 +0300

    hw

--- 4.1.py
+++ 4.1.py
@@ -1,17 +1,17 @@
 spisok = []
 spisok += '45'
-spisok.append (65)
+spisok.append(65)
 spisok += 'letters'
 spisok.append('summer')
-spisok.insert(1, [3,4,6])
+spisok.insert(1, [3, 4, 6])
 appendix = ['will', 'last', 'forever']
-print (spisok + appendix)
-print (spisok)
+print(spisok + appendix)
+print(spisok)
 spisok.extend(appendix)
-print (spisok)
-spisok.remove (65)
-spisok.pop (5)
+print(spisok)
+spisok.remove(65)
+spisok.pop(5)
 del spisok[0]
 spisok[1] = 10
-print (spisok.count ('e'))
-print (len(spisok))
+print(spisok.count('e'))
+print(len(spisok))
