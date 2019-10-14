99febf39ba4b42d921d8a86f0adee4e4affa1f5f
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2019-10-14 08:42:16 +0300

    Update 4..6.py

--- 4..6.py
+++ 4..6.py
@@ -1,7 +1,7 @@
 a = int(input('введите а'))
 b = int(input('введите в'))
 c = int(input('введите с'))
-if a + b < c or a + c < b or b + c < a:
+if a + b <= c or a + c <= b or b + c <= a:
     print('нет такого треугольника')
 elif a == b == c:
     print('равносторонний')
