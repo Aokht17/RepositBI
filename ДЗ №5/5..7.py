93c869cf0942cc1cd835074dafe43868aef56400
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2019-11-03 03:43:32 +0300

    Update 5..7.py

--- ДЗ №5/5..7.py
+++ ДЗ №5/5..7.py
@@ -1,8 +1,7 @@
 print('AGCT'.translate(str.maketrans('AGCT', 'TCGA')))
 
 dna = str(input('vvedite').upper().strip())
-dna.split()
 complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', 'U': 'A'}
-for s in dna:
+for s in dna[::-1]:
     print(complement.get(s, '-'), end='')
 
