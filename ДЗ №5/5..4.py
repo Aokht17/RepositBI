27ea480e4d0d1d02b246725c91cad90af118e96a
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2019-11-03 04:09:36 +0300

    Update 5..4.py

--- ДЗ №5/5..4.py
+++ ДЗ №5/5..4.py
@@ -1,5 +1,9 @@
 stroka = input('введите строку')
 if stroka[0] == ' ':
     print('строка начинается с пробела')
+elif stroka[0].isalpha():
+    print('строка начинается с буквы', stroka[0])
+elif stroka[0].isdigit():
+    print('строка начинается с цифры', stroka[0])
 else:
-    print('строка начинается с', stroka[0])
+    print('строка начинается с другого символа:', stroka[0])
