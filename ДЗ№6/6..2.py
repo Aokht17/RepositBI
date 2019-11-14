5494a5a964d062b5940bdcb5ebd5cac37d8b6ed5
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2019-11-14 23:52:59 +0300

    Update 6..2.py

--- ДЗ№6/6..2.py
+++ ДЗ№6/6..2.py
@@ -4,6 +4,6 @@ def formula_bine(n):
     :param n: non-negative integers
     :return: integer
     """
-    f = int((((1+5**0.5)/2)**(n-1)-((1-5**0.5)/2)**(n-1))/5**0.5)
+    f = int((((1+5**0.5)/2)**n-((1-5**0.5)/2)**n)/5**0.5)
     return f
 
