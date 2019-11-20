250d97a86b853b6f98257c4b80965726a1211ab8
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2019-11-20 13:36:20 +0300

    Update my_stupid_module.py

--- ДЗ№8/my_stupid_module.py
+++ ДЗ№8/my_stupid_module.py
@@ -1,16 +1,24 @@
 print('You managed to do this!')
 
 def tree():
+    """
+    draw a stupid christmas tree with * and O
+    """
     t = '*'
     for i in range(7):
         print(t.center(30,' '), end='\n')
         t += 'O**'
 
 def chitalka(b,i):
+    """
+    meaningless function
+    :param b: int
+    :param i: int
+    :return: a number and how many times it was printed
+    """
     y = 0
     while i <= 10:
         print(b+1, end='')
         y += 1
         i += 1
     return b+1, y
-
