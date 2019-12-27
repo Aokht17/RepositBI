9c9641d6d37ab6217dd8650a70e2957eaa807336
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2019-12-27 03:57:43 +0300

    Add files via upload

---
+++ 11..1.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr_1 = np.array([5, 8, 3, 4, 6], dtype='int')
+arr_2 = np.array([9, 2, 3, 7, 5], dtype=np.float16)
+arr_3 = np.full((3, 2), 7, dtype='str')
+arr_4 = np.ones((3, 3))
+arr_5 = np.arange(1, 6, 2)
+arr_6 = np.eye(3, 3)  #I have no idea where this really can be useful, but it’s very cool
