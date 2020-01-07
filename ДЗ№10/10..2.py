69081448ef69ff584170b7240df28d8b03c57ac2
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-01-07 19:45:08 +0300

    Update 10..2.py

--- ДЗ№10/10..2.py
+++ ДЗ№10/10..2.py
@@ -1,11 +1,13 @@
+import seaborn as sns
+from Bio import SeqIO
+
+
 def how_distributed(your_fasta):
     """
  creates a histogram and fit a kernel density estimate for distribution of lines' length in fasta file
     :param your_fasta: full path
     :return: none
     """
-    import seaborn as sns
-    from Bio import SeqIO
     u = list(SeqIO.parse(your_fasta, 'fasta'))
     app = []
     for line in u:
