60be62d6242a982173c6082fab5502d5529b9efe
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-01-09 01:13:38 +0300

    Update 10..3.py

--- ДЗ№10/10..3.py
+++ ДЗ№10/10..3.py
@@ -1,5 +1,7 @@
 import seaborn as sns
-np_matrix = list([i*0.5 for i in range(-100, 100, 2)])
+
+
+np_matrix = list([i for i in range(-50, 50, 2)])
 plot = sns.boxplot(data=np_matrix, color='yellow')
 plot.set(xlabel='x', ylabel='y', title='ящик с усами')
 print(plot)
