79a4cdae204df225eebf61b3891f1b1ba3d6ed56
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-01-09 17:03:44 +0300

    Add files via upload

---
+++ ДЗ№11/11..6.py
@@ -0,0 +1,18 @@
+from random import sample
+
+
+def still_readable(your_stroke):
+    """
+    prints your string with shuffled middle letters of each word in a sentence
+    :return: None
+    """
+    get_str = []
+    for word in your_stroke.split():
+        if len(word) > 3:
+            get_str.append(word[0] + ''.join(sample([i for i in word[1:-1]], len(word) - 2)) + word[-1])
+        else:
+            get_str.append(word)
+    print(*get_str)  # or you may use  > return ' '.join(words)
+
+
+still_readable('Здесь должна быть шутка про Штирлица')
