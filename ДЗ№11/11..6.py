dd75ae04ba6195e91500f6d7a22ca9449ad6ac5a
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-01-09 17:05:32 +0300

    Update 11..6.py

--- ДЗ№11/11..6.py
+++ ДЗ№11/11..6.py
@@ -1,13 +1,13 @@
 from random import sample
 
 
-def still_readable(your_stroke):
+def still_readable(your_string):
     """
     prints your string with shuffled middle letters of each word in a sentence
     :return: None
     """
     get_str = []
-    for word in your_stroke.split():
+    for word in your_string.split():
         if len(word) > 3:
             get_str.append(word[0] + ''.join(sample([i for i in word[1:-1]], len(word) - 2)) + word[-1])
         else:
