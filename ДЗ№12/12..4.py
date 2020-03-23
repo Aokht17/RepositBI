2db0b6196b36fc0605fbd7d9c41c311dac05c8ed
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-03-23 17:51:42 +0300

    Update 12..4.py

--- ДЗ№12/12..4.py
+++ ДЗ№12/12..4.py
@@ -7,12 +7,10 @@ def nucleotide_comb(n):
     :return: list
     """
     nucl = 'ATCG'
-    output = []
     for i in range(1, n+1):
-        output.extend(map("".join, product(nucl, repeat=i)))
-    yield output
+        for j in map("".join, product(nucl, repeat=i)):
+            yield j
 
 
 for o in nucleotide_comb(2):
     print(*o)
-
