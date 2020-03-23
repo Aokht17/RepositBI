54814368b03781cd729915bc9882e790d2046ccf
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-03-23 16:44:36 +0300

    Update 13..3.py

--- ДЗ№13/13..3.py
+++ ДЗ№13/13..3.py
@@ -1,29 +1,29 @@
 class MorePositive(set):
 
     def __init__(self, inp):
+        super().__init__()
         self.inp = inp
-        self.seq = set()
 
         for i in self.inp:
             if i > 0:
-                self.seq.add(i)
+                super().add(i)
 
-    def new_el(self, a):
+    def add(self, a):
         """
         adds a new positive element to the set
         """
         if a > 0:
-            self.seq.add(a)
+            super().add(a)
         else:
             raise ValueError
 
 
 d = MorePositive([1, 6, -7, 96, 0])
-MorePositive.new_el(d, 76)
-print(d.seq)
-MorePositive.new_el(d, 0)
 
 
-
-
-
+print(d)
+d = MorePositive([1, 6, -7, 96, 0])
+MorePositive.add(d, 76)
+d.add(90)
+print(d)
+print(d.difference([1, 2]))
