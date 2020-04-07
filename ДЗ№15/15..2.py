2d0e84919b3db1c4ee187f3a5f9acd330def0e01
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-04-08 02:23:17 +0300

    Update 15..2.py

--- ДЗ№15/15..2.py
+++ ДЗ№15/15..2.py
@@ -1,7 +1,7 @@
 import re
 
 with open('2430AD.txt', 'r') as file:
-    pat = re.compile(r'(\d+)')
+    pat = re.compile(r'(?<![A-Za-z0-9.])\d+\.*\d+(?!\w)')
     num=[]
     for line in file:
         num += pat.findall(line)
