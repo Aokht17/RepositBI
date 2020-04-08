41e33f6c6099fa30e66323a89a2cd73a2bd54de6
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-04-08 14:45:36 +0300

    Add files via upload

---
+++ ДЗ№15/15.1.py
@@ -0,0 +1,11 @@
+import re
+
+with open('references.txt', 'r') as file, open('ftps', 'w') as urls:
+    pat = re.compile(r'(?!\W)ftp\.[A-Za-z0-9./_]+')
+    for lines in file:
+        for line in pat.findall(lines):
+            urls.write(line)
+            urls.write("\n")
+    
+
+
