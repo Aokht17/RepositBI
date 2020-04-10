7ece206cd8d7f31db06f1c7e6acd5db3b34ddba5
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-04-10 17:39:38 +0300

    Update 15..1.py

--- ДЗ№15/15..1.py
+++ ДЗ№15/15..1.py
@@ -1,7 +1,7 @@
 import re
 
 with open('references.txt', 'r') as file, open('ftps', 'w') as urls:
-    pat = re.compile(r'(?!\W)ftp\.[A-Za-z0-9./_]+')
+    pat = re.compile(r'(?!\W)ftp\.[A-Za-z0-9./_#]+')
     for lines in file:
         for line in pat.findall(lines):
             urls.write(line)
