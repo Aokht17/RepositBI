993bb21aea842b4a9e41bce08aef4efc8c51157a
Aokht17 <56379827+Aokht17@users.noreply.github.com>
2020-04-10 17:55:00 +0300

    Update 15..5.py

--- ДЗ№15/15..5.py
+++ ДЗ№15/15..5.py
@@ -2,13 +2,13 @@ import re
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 with open('2430AD.txt', 'r') as file:
     pat = re.compile(r'[A-Za-z]\.?\w*')  # ok, my parser cut 'didn't' as 'didn' and 't', it's not alright
-    words = []
+    words = set()
     for line in file:
         for i in pat.findall(line):
-            if i.lower() not in words:
-                words.append(i.lower())
+            words.add(i.lower())
 
 lens = [len(i) for i in words]
 ax = sns.distplot(tuple(lens))
